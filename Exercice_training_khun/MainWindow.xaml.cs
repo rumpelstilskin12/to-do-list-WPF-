fc76880c04cd85d2a23fc5cad7df8566d1853148
Walid <BARCA-WAWA@hotmail.com>
2021-04-15 10:11:02 +0200

    initial commit

---
+++ Exercice_training_khun/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercice_training_khun
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        //Vu que c'est de la logique visuel , on peut le faire dans le code bin
+        private void SetFocus(object sender , RoutedEventArgs e)
+        {
+            TBEntry.Focus();
+        }
+
+      
+    }
+}
---
+++ Exercice_training_khun/ViewModels/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using ToolBox.MVVM.Base;
+using ToolBox.MVVM.Commands;
+
+namespace Exercice_training_khun.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        private string _entry;
+
+        public string Entry
+        {
+            get
+            {
+                return _entry;
+            }
+
+            set
+            {
+                _entry = value;
+                OnPropertyChanged();
+                //prévenir le bouton que mon entry a changé et qu'il doit se mettre à jour
+                AddCommand.OnCanExecuteChanged();
+            }
+        }
+
+        private ObservableCollection<string> _list;
+
+        public ObservableCollection<string> List
+        {
+            get { return _list; }
+            set { _list = value; OnPropertyChanged(); }
+        }
+
+
+        public CommandBase AddCommand { get; set; }
+
+
+        public MainViewModel()
+        {
+
+            AddCommand = new CommandBase(Add,CanAdd);
+            List = new ObservableCollection<string>() { "Fulcrum ", "Obiwan Kenobi", "Ahsoka tano" };
+        }
+
+        private void Add()
+        {
+            List.Add(Entry);
+            //permet de vider le textBox , uniquement si on retrouve la methode OnPropertyChanged() au niveau du entry.
+            Entry = null;
+        }
+        
+        private bool CanAdd()
+        {
+            return !string.IsNullOrEmpty(Entry);
+        }
+        
+
+    }
+}
